e410b51f16b7eed2831cb5f6e45e2c9834c819b6
Nikita Rana <105151978+rebryhm@users.noreply.github.com>
2025-03-29 22:21:50 +0530

    Update TkinterCalculator.py

--- TkinterCalculator.py
+++ TkinterCalculator.py
@@ -1,26 +1,3 @@
-# from tkinter import *
-# window = Tk()  #title of our window
-# #inp = Label(window, text = "hello word")
-
-# #now we are adding a title 
-# window.title("simple")
-# window.geometry("500x700")
-# window.config(bg = "yellow")
-
-# frame1 = Frame(window , bg = "red" , width = 300 , height = 300)
-# frame2 = Frame(window , bg = "green" , width = 300 , height = 300)
-# frame1.pack(side=TOP)
-# frame2.pack(side=BOTTOM)
-
-# # button1 = Button(frame1 , text = "Button1" , bg = "blue")
-# # button2 = Button(frame2 , text = "Button1" , bg = "light green")
-
-# # button1.pack()
-# # button2.pack()
-# # button3.pack()
-# #inp.pack()
-# window.mainloop()
-
 from tkinter import *
 window = Tk()
 window.geometry('500x500')
